86b8d920e7a441be8bacaed154cd8ae2fb69d63b
Sivabalan-19 <pmsiva.1906@gmail.com>
2026-03-10 09:26:15 +0530

    register added

--- index.js
+++ index.js
@@ -7,13 +7,21 @@ const logger = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
 const routes = require('./routes');
 
-const allowedOrigin = process.env.CLIENT_URL || 'http://localhost:3000';
+const allowedOrigins = (process.env.CLIENT_URL || 'http://localhost:3000')
+  .split(',')
+  .map((o) => o.trim());
 
 const corsOptions = {
-	origin: allowedOrigin,
-	credentials: true,
-	methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-	allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error(`CORS: origin ${origin} not allowed`));
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
 db.connect();
